2b6105e2e8718609f9036077b147ce0f1138220a
Andrey Spiridonov <andrey@h2o.ai>
2018-10-05 18:26:04 +0200

    PUBDEV-5956: Make it users's responsibility to remove keys for cv fold assignments .

--- h2o-automl/src/test/java/ai/h2o/automl/AutoMLTest.java
+++ h2o-automl/src/test/java/ai/h2o/automl/AutoMLTest.java
@@ -9,6 +9,8 @@ import water.fvec.Frame;
 
 import java.util.Date;
 
+import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.assertNull;
 import static junit.framework.TestCase.assertTrue;
 
 public class AutoMLTest extends TestUtil {
@@ -39,7 +41,7 @@ public class AutoMLTest extends TestUtil {
     }
   }
 
-  @Test public void KeepCrossValidationFoldAssignmentTest() {
+  @Test public void KeepCrossValidationFoldAssignmentEnabledTest() {
     AutoML aml = null;
     Frame fr = null;
     Model leader = null;
@@ -59,11 +61,40 @@ public class AutoMLTest extends TestUtil {
       leader = aml.leader();
 
       assertTrue(leader !=null && leader._parms._keep_cross_validation_fold_assignment);
-      assertTrue(leader !=null && leader._output._cross_validation_fold_assignment_frame_id != null);
+      assertNotNull(leader._output._cross_validation_fold_assignment_frame_id);
 
     } finally {
       // Since user asked to keep cv fold assignments( we set parameter `keep_cross_validation_fold_assignment` to true) we need to remove this key manually
-      DKV.remove(leader._output._cross_validation_fold_assignment_frame_id);
+      Frame cvFoldAssignmentFrame = DKV.getGet(leader._output._cross_validation_fold_assignment_frame_id);
+      cvFoldAssignmentFrame.delete();
+      if(aml!=null) aml.deleteWithChildren();
+      if(fr != null) fr.remove();
+    }
+  }
+
+  @Test public void KeepCrossValidationFoldAssignmentDisabledTest() {
+    AutoML aml = null;
+    Frame fr = null;
+    Model leader = null;
+    try {
+      AutoMLBuildSpec autoMLBuildSpec = new AutoMLBuildSpec();
+      fr = parse_test_file("./smalldata/airlines/allyears2k_headers.zip");
+      autoMLBuildSpec.input_spec.training_frame = fr._key;
+      autoMLBuildSpec.input_spec.response_column = "IsDepDelayed";
+      autoMLBuildSpec.build_control.stopping_criteria.set_max_models(1);
+      autoMLBuildSpec.build_control.stopping_criteria.set_max_runtime_secs(30);
+      autoMLBuildSpec.build_control.keep_cross_validation_fold_assignment = false;
+
+      aml = AutoML.makeAutoML(Key.<AutoML>make(), new Date(), autoMLBuildSpec);
+      AutoML.startAutoML(aml);
+      aml.get();
+
+      leader = aml.leader();
+
+      assertTrue(leader !=null && !leader._parms._keep_cross_validation_fold_assignment);
+      assertNull(leader._output._cross_validation_fold_assignment_frame_id);
+
+    } finally {
       if(aml!=null) aml.deleteWithChildren();
       if(fr != null) fr.remove();
     }
